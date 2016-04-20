5e2034167421878805508a8810cd21d47255e145
name <freiny50@gmail.com>
2016-04-19 21:45:45 -0500

    Add support for undefined callbacks. Place-holder functions get executed.

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ import (
 	gw "github.com/freiny/go-window"
 )
 
-var cfg = gw.Config{}
-var cb = gw.Callbacks{}
+// Global variables relative to user application
+type Global struct {
+	wc   gw.WinConfig
+	cb   gw.Callbacks
+	cur  gw.Cursor
+	img1 *image.RGBA
+	img2 *image.RGBA
+}
 
-var img1, img2 *image.RGBA
+var g = Global{}
 
 func main() {
 
@@ -26,28 +32,26 @@ func main() {
 		resPath = "Resources/"
 	}
 
-	img1, _ = gw.GetImage(resPath + "k1.png")
-	img2, _ = gw.GetImage(resPath + "k2.png")
+	g.img1, _ = gw.GetImage(resPath + "k1.png")
+	g.img2, _ = gw.GetImage(resPath + "k2.png")
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	// cfg.Width = 1280
-	// cfg.Height = 720
-	cfg.Width = 8 * 64
-	cfg.Height = 8 * 64
-	cfg.X = 0
-	cfg.Y = 0
+	g.wc = gw.WinConfig{
+		W: 8 * 64,
+		H: 8 * 64,
+		X: 0,
+		Y: 0,
+	}
+	g.cb = gw.Callbacks{
+		// Render: onRender,
+		// CursorMove: onCursorMove,
+		// Key:        onKey,
+		FPS: onFPS,
+	}
 
-	cb.Render = onRender
-	cb.Key = onKey
-	cb.CursorMove = onCursorMove
-	cb.FPS = onFPS
-
-	// img1 = gw.RandomImage(image.Point{cfg.Width, cfg.Height})
-	// img2 = gw.RandomImage(image.Point{cfg.Width, cfg.Height})
-
-	// gw.SetDebug()
-	gw.Init(cfg, cb)
+	gw.SetDebug()
+	gw.Init(g.wc, g.cb)
 }
 
 func onFPS(fps int) {
@@ -63,33 +67,18 @@ func onKey(w *gw.Window, key gw.Key, scancode int, action gw.Action, mods gw.Mod
 	}
 }
 
-var xCursor, yCursor float64
-
-func onCursorMove(xPos float64, yPos float64) {
-	xCursor, yCursor = xPos, yPos
-	// fmt.Println("CURSOR: ", xCursor, yCursor)
+func onCursorMove(c gw.Cursor) {
+	g.cur = c
 }
-
-var r = 0
 
 func onRender() *image.RGBA {
 
 	// xCur, yCur := int(xCursor), int(yCursor)
-	var rgba = image.NewRGBA(image.Rect(0, 0, cfg.Width, cfg.Height))
-	if gw.Toggle() {
-		rgba = img1
-		// rgba = gw.RandomImage(image.Point{cfg.Width, cfg.Height})
-		// rgba = gw.ClearImage(rgba, color.RGBA{255, 0, 0, 255})
-		// rgba, _ = gw.GetImage("black.png")
-
+	var rgba = image.NewRGBA(image.Rect(0, 0, g.wc.W, g.wc.Y))
+	if gw.SetFrameToggle() {
+		rgba = g.img1
 	} else {
-		rgba = img2
-		// Hw()
-		// var a image.RGBA
-		// rgba = gw.RandomImage(image.Point{cfg.Width, cfg.Height})
-
-		// rgba = gw.ClearImage(rgba, color.RGBA{0, 255, 0, 255})
-		// rgba, _ = gw.GetImage("black.png")
+		rgba = g.img2
 	}
 
 	return rgba
